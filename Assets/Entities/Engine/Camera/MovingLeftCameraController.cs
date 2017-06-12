b03b246260962dbcab8c3a6980fb5c1d3bb6d2f9
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2017-06-12 12:23:24 -0500

    Death Camera Camera moves to the left at given speed, with death box on left side. if player gets outside of screen they die.

---
+++ Assets/Entities/Engine/Camera/MovingLeftCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MovingLeftCameraController : MonoBehaviour {
+
+	public GameObject protagonist;
+	public float moveSpeed;
+	private CharacterController controller;
+	public float endPosition;
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		protagonist = GameObject.Find ("Character");
+		controller = GetComponent<CharacterController>();
+		velocity = new Vector3(moveSpeed, 0, 0);
+	}
+
+	void Update () {
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
